37e75384ae33a7753376e55329e8bf2e514c14f2
rayhan <rayhananthaakbar@gmail.com>
2025-08-19 12:18:15 +0700

    feat: add api url and admin token

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,8 @@
 const appConfig = {
-    qiscusApiURL: "https://omnichannel.qiscus.com/api/v2",
+    qiscusApiURL: "https://omnichannel.qiscus.com/api",
     qiscusKey: process.env.QISCUS_KEY!,
     qiscusAppId: process.env.QISCUS_APP_ID!,
+    qiscusAdminToken: process.env.QISCUS_ADMIN_TOKEN!,
 };
 
 export default appConfig;
