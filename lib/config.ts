473546252de60b3b98965c296273631100509b46
rayhan <rayhananthaakbar@gmail.com>
2025-08-19 10:59:11 +0700

    feat: add app config

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+const appConfig = {
+    qiscusApiURL: "https://omnichannel.qiscus.com/api/v2",
+    qiscusKey: process.env.QISCUS_KEY!,
+    qiscusAppId: process.env.QISCUS_APP_ID!,
+};
+
+export default appConfig;
