dd17b5f9a372f90ed22b265e97ff390dac440a1d
rayhan <rayhananthaakbar@gmail.com>
2025-08-19 11:00:44 +0700

    feat: add mark resolved webhook

---
+++ app/api/channel/mark_resolved/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const data = await req.json();
+
+    console.log("=====================\nMARK AS RESOLVED\n=====================");
+    console.log(data);
+
+    return NextResponse.json(data, { status: 200 });
+}
