7a5ace4b8d3736b9ac71202ecafc57b9df5e1598
rayhan <rayhananthaakbar@gmail.com>
2025-08-19 12:18:25 +0700

    feat: add pre-made response payload

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,16 @@
+import { NextResponse } from "next/server";
+
 export const parseStringify = (value: any) => {
     return JSON.parse(JSON.stringify(value));
+};
+
+export const responsePayload = (status: "ok" | "invalid" | "error", message: string, payload: any, statusCode: number) => {
+    return NextResponse.json(
+        {
+            status: status,
+            message: message,
+            payload: payload,
+        },
+        { status: statusCode }
+    );
 };
