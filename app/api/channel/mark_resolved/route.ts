226d0c874ea67769ac91f178ddb3b1a13e200c82
rayhan <rayhananthaakbar@gmail.com>
2025-08-19 12:56:42 +0700

    feat: update status as resolved in db

--- app/api/channel/mark_resolved/route.ts
+++ app/api/channel/mark_resolved/route.ts
@@ -1,10 +1,23 @@
-import { NextResponse } from "next/server";
+import { updateRoomStatus } from "@/lib/rooms";
+import { responsePayload } from "@/lib/utils";
 
 export async function POST(req: Request) {
     const data = await req.json();
 
     console.log("=====================\nMARK AS RESOLVED\n=====================");
-    console.log(data);
 
-    return NextResponse.json(data, { status: 200 });
+    try {
+        const {
+            resolved_by: { id: agent_id },
+            service: { room_id },
+        } = data;
+
+        const resolved = await updateRoomStatus({ room_id, agent_id, status: "RESOLVED" });
+        console.log(resolved[0]);
+
+        return responsePayload("ok", `success mark as resolved room ${room_id}`, {}, 200);
+    } catch (error) {
+        console.error("Failed to mark resolved room", error);
+        return responsePayload("error", "internal server error", {}, 500);
+    }
 }
