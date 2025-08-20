87f57cc5b61266744ba9b013ea98ec1d1e179159
rayhan <rayhananthaakbar@gmail.com>
2025-08-20 15:53:51 +0700

    refactor: parameters

--- lib/qiscus.ts
+++ lib/qiscus.ts
@@ -12,35 +12,40 @@ import { parseStringify } from "./utils";
  * @param {Object<string, number>} [params.agentLoads] - Optional map of agent IDs to their current load.
  * @returns {Promise<Agent[]>} A promise that resolves to a list of available agents.
  */
-export const getAvailableAgents = async ({ room_id, agentLoads }: { room_id: string; agentLoads?: Record<string, number> }): Promise<Agent[]> => {
-    const res = await axios.get(`${appConfig.qiscusApiURL}/v2/admin/service/available_agents`, {
-        params: {
-            room_id: room_id,
-        },
-        headers: {
-            Authorization: appConfig.qiscusAdminToken,
-            "Qiscus-App-Id": appConfig.qiscusAppId,
-        },
-    });
+export const getAvailableAgents = async ({ roomId, agentLoads }: { roomId: string; agentLoads?: Record<string, number> }): Promise<Agent[]> => {
+    try {
+        const res = await axios.get(`${appConfig.qiscusApiURL}/v2/admin/service/available_agents`, {
+            params: {
+                room_id: roomId,
+            },
+            headers: {
+                Authorization: appConfig.qiscusAdminToken,
+                "Qiscus-App-Id": appConfig.qiscusAppId,
+            },
+        });
 
-    const agents: any[] = (await res.data.data.agents) || [];
+        const agents: any[] = (await res.data.data.agents) || [];
 
-    const availableAgents = agents
-        .filter((agent) => {
-            const liveCount = Number(agent.current_customer_count) || 0;
-            const localCount = agentLoads?.[agent.id] || 0;
-            const totalCount = liveCount + localCount;
+        const availableAgents = agents
+            .filter((agent) => {
+                const liveCount = Number(agent.current_customer_count) || 0;
+                const localCount = agentLoads?.[agent.id] || 0;
+                const totalCount = liveCount + localCount;
 
-            return agent.is_available && totalCount < appConfig.maxCustomers;
-        })
-        .map(({ id, email, name, current_customer_count }) => ({
-            id,
-            email,
-            name,
-            customerCount: Number(current_customer_count),
-        }));
+                return agent.is_available && totalCount < appConfig.maxCustomers;
+            })
+            .map(({ id, email, name, current_customer_count }) => ({
+                id,
+                email,
+                name,
+                customerCount: Number(current_customer_count),
+            }));
 
-    return parseStringify(availableAgents);
+        return parseStringify(availableAgents);
+    } catch (error) {
+        console.error(error, "Failed to fetch available agents");
+        return [];
+    }
 };
 
 /**
@@ -51,21 +56,25 @@ export const getAvailableAgents = async ({ room_id, agentLoads }: { room_id: str
  * @param {string} params.agent_id - The ID of the agent being assigned.
  */
 
-export const assignAgent = async ({ room_id, agent_id }: { room_id: string; agent_id: string }) => {
-    await axios.post(
-        `${appConfig.qiscusApiURL}/v1/admin/service/assign_agent`,
-        {
-            room_id: room_id,
-            agent_id: agent_id,
-            replace_latest_agent: false,
-            max_agent: 1,
-        },
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Qiscus-Secret-Key": appConfig.qiscusKey,
-                "Qiscus-App-Id": appConfig.qiscusAppId,
+export const assignAgent = async ({ roomId, agentId }: { roomId: string; agentId: string }) => {
+    try {
+        await axios.post(
+            `${appConfig.qiscusApiURL}/v1/admin/service/assign_agent`,
+            {
+                room_id: roomId,
+                agent_id: agentId,
+                replace_latest_agent: false,
+                max_agent: 1,
             },
-        }
-    );
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Qiscus-Secret-Key": appConfig.qiscusKey,
+                    "Qiscus-App-Id": appConfig.qiscusAppId,
+                },
+            }
+        );
+    } catch (error) {
+        console.error(error, "Failed to assign an agent");
+    }
 };
