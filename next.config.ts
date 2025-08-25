16641d6c620057b55d08c63f781f119ef97c8573
rayhan <rayhananthaakbar@gmail.com>
2025-08-25 11:27:16 +0700

    feat: add ngrok dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    allowedDevOrigins: ["bull-still-externally.ngrok-free.app"],
 };
 
 export default nextConfig;
