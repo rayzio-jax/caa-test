0c4d8ed7be0af0aca15131a1ab67870610016fd1
rayhan <rayhananthaakbar@gmail.com>
2025-08-28 18:16:02 +0700

    chore: schema type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,7 @@
 declare interface Room {
-    id: string;
-    room_id: number;
-    channel_id: number;
-    agent_id?: number;
+    id: number;
+    channelId: number;
+    agentId: number;
     status: "QUEUE" | "HANDLED" | "RESOLVED";
     created_at: Date;
     updated_at: Date;
